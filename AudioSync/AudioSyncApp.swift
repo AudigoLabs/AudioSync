e30c7b59abf7c4bac23c770ec64364320dc30dc3
Brian Gomberg <brian@audigolabs.com>
2021-10-12 10:32:08 -0700

    Initial Commit

---
+++ AudioSync/AudioSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioSyncApp.swift
+//  AudioSync
+//
+//  Created by Brian Gomberg on 10/12/21.
+//
+
+import SwiftUI
+
+@main
+struct AudioSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioSync/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AudioSync
+//
+//  Created by Brian Gomberg on 10/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
