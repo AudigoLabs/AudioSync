f2dbe51938d7ab18536fa70aa9e2ca499a981124
Brian Gomberg <brian@audigolabs.com>
2021-10-14 14:46:03 -0700

    More work

--- AudioSync/AudioManager.swift
+++ AudioSync/AudioManager.swift
@@ -14,28 +14,44 @@ class AudioManager: NSObject, AVAudioRecorderDelegate, ObservableObject {
     private static let ASSETS_DIR_URL = DOC_DIR_URL.appendingPathComponent("assets", isDirectory: true)
     private static let BACKING_FILE_URL = ASSETS_DIR_URL.appendingPathComponent("backing.wav")
     private static let OVERLAY_FILE_URL = ASSETS_DIR_URL.appendingPathComponent("overlay.wav")
-    private static let SCHEDULE_DELAY = 0.1
+    private static let SCHEDULE_DELAY = 0.33
+
+    private static let SECONDS_TO_TICKS: Double = {
+        var tinfo = mach_timebase_info()
+        mach_timebase_info(&tinfo)
+        let timecon = Double(tinfo.denom) / Double(tinfo.numer)
+        return timecon * 1_000_000_000
+    }()
+
+    private var systemIoBufferDuration: TimeInterval {
+        return AVAudioSession.sharedInstance().ioBufferDuration
+    }
+    private var systemInputLatency: TimeInterval {
+        return AVAudioSession.sharedInstance().inputLatency
+    }
+    private var systemOutputLatency: TimeInterval {
+        return AVAudioSession.sharedInstance().outputLatency
+    }
 
     @Published private(set) var isRecording: Bool = false
-    @Published private(set) var hasBackingTrack: Bool = false
+    @Published private(set) var hasBackingTrack: Bool = true
 
     private var recorder: AVAudioRecorder!
-    private var playbackEngine = AVAudioEngine()
-    private var playbackPlayerNode = AVAudioPlayerNode()
+    private var audioEngine = AVAudioEngine()
+    private var playerNode = AVAudioPlayerNode()
     private var playbackAudioFile: AVAudioFile! = nil
-    private var playbackOffset: TimeInterval = 0.0
-    private var playbackOutputLatency: TimeInterval = 0.0
+    private var currentStartTime: TimeInterval = 0.0
 
     private override init() {
         super.init()
     }
 
     public func setup(completion: @escaping () -> Void) {
-        // Create the assets directory and remove any existing files
-        try! FileManager.default.createDirectory(at: Self.ASSETS_DIR_URL, withIntermediateDirectories: true, attributes: nil)
-        try? FileManager.default.removeItem(at: Self.BACKING_FILE_URL)
-        try? FileManager.default.removeItem(at: Self.OVERLAY_FILE_URL)
-        print("Documents path: \(Self.DOC_DIR_URL)")
+        guard FileManager.default.fileExists(atPath: Self.BACKING_FILE_URL.path) else {
+            fatalError("Backing track doesn't exist")
+        }
+
+        playbackAudioFile = try! AVAudioFile(forReading: Self.BACKING_FILE_URL)
 
         // Create the recording session
         let session = AVAudioSession.sharedInstance()
@@ -54,81 +70,12 @@ class AudioManager: NSObject, AVAudioRecorderDelegate, ObservableObject {
         try! session.setActive(true, options: [])
         print("Audio session active")
 
-        playbackEngine.attach(playbackPlayerNode)
-        playbackEngine.connect(playbackPlayerNode, to: playbackEngine.mainMixerNode, format: nil)
-    }
+        print("IO Buffer: \(systemIoBufferDuration)")
+        print("Input latency: \(systemInputLatency)")
+        print("Output latency: \(systemOutputLatency)")
 
-    public func startRecording() {
-        guard !isRecording else {
-            fatalError("Already recording")
-        }
-
-        if hasBackingTrack {
-            // Start playback of the backing track
-            playbackAudioFile = try! AVAudioFile(forReading: Self.BACKING_FILE_URL)
-
-            print("Starting playback")
-            try! playbackEngine.start()
-
-            playbackOutputLatency = AVAudioSession.sharedInstance().outputLatency
-            print("playbackOutputLatency: \(playbackOutputLatency)")
-
-            var delayTime = Self.SCHEDULE_DELAY
-            let adjustedTimeOffset = playbackOffset
-            var startingFrame: AVAudioFramePosition = 0
-            let outputFormat = playbackPlayerNode.outputFormat(forBus: 0)
-            if adjustedTimeOffset < 0 {
-                // negative offset, so calculate non-zero starting frame:
-                let startingTime = abs(adjustedTimeOffset)
-                startingFrame = AVAudioFramePosition(outputFormat.sampleRate * startingTime)
-            } else {
-                // positive (or zero) offset, so calculate delay time
-                delayTime += adjustedTimeOffset
-            }
-
-            //delay the playback of the initial buffer so that we're not trying to play immediately when the engine starts
-
-            var tinfo = mach_timebase_info()
-            mach_timebase_info(&tinfo)
-            let timecon = Double(tinfo.denom) / Double(tinfo.numer)
-            let secondsToTicks = timecon * 1_000_000_000
-            let delay = 0.33 * secondsToTicks
-            let startTime = AVAudioTime(hostTime: mach_absolute_time() + UInt64(delay))
-
-//            let lastRenderTime = playbackPlayerNode.lastRenderTime!
-//            let lastRenderTimeInSamples = lastRenderTime.sampleTime
-//            let startSampleTime = lastRenderTimeInSamples + AVAudioFramePosition(delayTime * outputFormat.sampleRate)
-//            let startTime = AVAudioTime(sampleTime: startSampleTime, atRate: outputFormat.sampleRate)
-
-            var frameCount = AVAudioFrameCount(playbackAudioFile.length)
-            frameCount -= UInt32(exactly: startingFrame) ?? 0
-
-            print("arrangmentTimeOffset: \(playbackOffset)")
-            print("adjustedTimeOffset: \(adjustedTimeOffset)")
-            print("startingFrame: \(startingFrame)")
-            print("delayTime: \(delayTime)")
-            print("startTime: \(Double(startTime.hostTime)/secondsToTicks)")
-            print("frameCount: \(frameCount)")
-
-            var isFirstOutput = true
-            let outFormat = playbackEngine.mainMixerNode.outputFormat(forBus: 0)
-            playbackEngine.mainMixerNode.installTap(onBus: 0, bufferSize: 1024, format: outFormat) { (pcmBuffer, timestamp) in
-                if isFirstOutput {
-                    print("OUT TIME", Double(timestamp.hostTime)/secondsToTicks)
-                    isFirstOutput = false
-                }
-            }
-
-            playbackPlayerNode.scheduleSegment(playbackAudioFile, startingFrame: startingFrame, frameCount: frameCount, at: nil, completionCallbackType: .dataPlayedBack) { _ in
-                DispatchQueue.main.async {
-                    self.playbackPlayerNode.stop()
-                    print("Backing track completed playback")
-                }
-            }
-            playbackPlayerNode.prepare(withFrameCount: frameCount)
-
-            playbackPlayerNode.play(at: startTime)
-        }
+        audioEngine.attach(playerNode)
+        audioEngine.connect(playerNode, to: audioEngine.mainMixerNode, format: audioEngine.mainMixerNode.inputFormat(forBus: 0))
 
         let settings: [String:Any] = [
             AVFormatIDKey: kAudioFormatLinearPCM,
@@ -140,12 +87,33 @@ class AudioManager: NSObject, AVAudioRecorderDelegate, ObservableObject {
             AVLinearPCMIsFloatKey: false,
         ]
         try? FileManager.default.removeItem(at: Self.OVERLAY_FILE_URL)
-        recorder = try! AVAudioRecorder(url: hasBackingTrack ? Self.OVERLAY_FILE_URL : Self.BACKING_FILE_URL, settings: settings)
+        recorder = try! AVAudioRecorder(url: Self.OVERLAY_FILE_URL, settings: settings)
         recorder.delegate = self
-        guard recorder.record() else {
+    }
+
+    public func startRecording() {
+        guard !isRecording else {
+            fatalError("Already recording")
+        }
+
+        let frameCount = playbackAudioFile.length
+        try! audioEngine.start()
+
+        // Delay the playback of the initial buffer so that we're not trying to play immediately when the engine starts
+        let audioStartTime = AVAudioTime(hostTime: mach_absolute_time() + UInt64((Self.SCHEDULE_DELAY - systemOutputLatency) * Self.SECONDS_TO_TICKS))
+        currentStartTime = Double(audioStartTime.hostTime) / Self.SECONDS_TO_TICKS
+
+        playerNode.play()
+        playerNode.scheduleSegment(playbackAudioFile, startingFrame: 0, frameCount: AVAudioFrameCount(frameCount), at: audioStartTime) {
+            DispatchQueue.main.async {
+                self.playerNode.stop()
+                print("Backing track completed playback")
+            }
+        }
+
+        guard recorder.record(atTime: recorder.deviceCurrentTime + Self.SCHEDULE_DELAY - systemInputLatency) else {
             fatalError("Failed to start recording")
         }
-        print(Date().timeIntervalSince1970, recorder.deviceCurrentTime, recorder.currentTime)
         isRecording = true
     }
 
@@ -154,18 +122,13 @@ class AudioManager: NSObject, AVAudioRecorderDelegate, ObservableObject {
             fatalError("Not recording")
         }
 
-        let playbackTime = hasBackingTrack ? currentAbsoluteTime() : nil
-        let recorderTime = recorder.currentTime
-        print(Date().timeIntervalSince1970, recorder.deviceCurrentTime, recorderTime, playbackTime)
-        print(AVAudioSession.sharedInstance().inputLatency, AVAudioSession.sharedInstance().outputLatency, AVAudioSession.sharedInstance().ioBufferDuration)
-        print(playbackPlayerNode.outputPresentationLatency, playbackPlayerNode.latency)
-        print(playbackEngine.mainMixerNode.latency, playbackEngine.mainMixerNode.outputPresentationLatency)
-
+        let recordStartTime = recorder.deviceCurrentTime - recorder.currentTime
         recorder.stop()
-        recorder = nil
         isRecording = false
-        hasBackingTrack = true
-        playbackAudioFile = nil
+
+        print("Started at: \(currentStartTime)")
+        print("Recording actually started at: \(recordStartTime)")
+        print("Recording offset: \(recordStartTime - currentStartTime)")
     }
 
     func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
@@ -181,28 +144,5 @@ class AudioManager: NSObject, AVAudioRecorderDelegate, ObservableObject {
             return
         }
         print("Recorder encode error: \(error)")
-    }
-
-    private func currentAbsoluteTime() -> TimeInterval? {
-        guard let nodeTime = playbackPlayerNode.lastRenderTime, let playerTime = playbackPlayerNode.playerTime(forNodeTime: nodeTime) else {
-            return nil
-        }
-
-        let trackTime = Double(playerTime.sampleTime) / playerTime.sampleRate
-
-        // If arrangementTimeOffset > 0, add to arrangementTime to get "absolute" time.
-        // Do not add if negative (ie subtract), due to how AVAudioPlayerNodes are scheduled:
-        var arrangementTime = trackTime + max(0, playbackOffset)
-
-        // Account for the output latency (based on the value at the start of recording to keep things simple for now)
-        arrangementTime -= playbackOutputLatency
-
-        // `arrangementTime` may be negative, due to the player being scheduled in the future.
-        // Return nil until non-negative:
-        guard arrangementTime >= 0 else {
-            return nil
-        }
-
-        return arrangementTime
     }
 }
